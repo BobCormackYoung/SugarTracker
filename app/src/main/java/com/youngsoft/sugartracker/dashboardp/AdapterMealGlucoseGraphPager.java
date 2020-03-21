313d009a867b45d091922780320cc4bd9010dc48
Pauhoihoi <rob.cormack@gmail.com>
2020-03-21 23:51:42 +0100

    graphs correctly displayed for each viewPager

--- app/src/main/java/com/youngsoft/sugartracker/dashboardp/AdapterMealGlucoseGraphPager.java
+++ app/src/main/java/com/youngsoft/sugartracker/dashboardp/AdapterMealGlucoseGraphPager.java
@@ -1,11 +1,16 @@
 package com.youngsoft.sugartracker.dashboardp;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.youngsoft.sugartracker.UtilMethods;
+
+import java.util.Calendar;
 
 /**
  * Adapter for horizontal scrolling viewPager
@@ -16,22 +21,43 @@ public class AdapterMealGlucoseGraphPager extends FragmentStatePagerAdapter {
     int num_items = 5;
     long startDate;
     long endDate;
+    int numDays;
 
-    public AdapterMealGlucoseGraphPager(@NonNull FragmentManager fm, int behavior) {
+    public AdapterMealGlucoseGraphPager(@NonNull FragmentManager fm, int behavior, int numDays) {
         super(fm, behavior);
+        this.numDays = numDays;
+        Log.i("AMGGP const","numDays: " + numDays);
     }
 
     @NonNull
     @Override
     public Fragment getItem(int position) {
+
+        Log.i("AMGGP getItem","numDays: " + numDays);
+
+        //set the latest date based on the position in the scrollView & number of items
+        Calendar latestDate = Calendar.getInstance();
+        latestDate = UtilMethods.setCalendarToEndOfDay(latestDate);
+        latestDate.add(Calendar.DATE, (position+1-num_items)*numDays);
+        Log.i("AMGGP getItem", "LD: " + UtilMethods.convertDate(latestDate.getTimeInMillis(),"yyyy-MM-dd HH:mm:ss"));
+
+        //set the earliest date of the range to be displayed
+        Calendar earliestDate = Calendar.getInstance();
+        earliestDate.setTimeInMillis(latestDate.getTimeInMillis());
+        earliestDate.add(Calendar.DATE,-numDays-1);
+        earliestDate.add(Calendar.MILLISECOND,2);
+        Log.i("AMGGP getItem", "ED: " + UtilMethods.convertDate(earliestDate.getTimeInMillis(),"yyyy-MM-dd HH:mm:ss"));
+
+        //instantiate the new fragment
         Fragment fragment = new FragmentMealGlucoseGraphPager();
+
+        //send data to the fragment as arguments
         Bundle args = new Bundle();
         args.putInt("position" , position);
+        args.putInt("numDays", numDays);
+        args.putLong("earliestDate", earliestDate.getTimeInMillis());
+        args.putLong("latestDate", latestDate.getTimeInMillis());
         fragment.setArguments(args);
-
-        //TODO: instantiate new fragment
-        //TODO: provide start & end dates as argument
-        //TODO: return fragment
 
         return fragment;
     }
